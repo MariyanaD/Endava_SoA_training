d9b71e468c2489dfb6d075eefb827e12451f20e8
MariyanaD <m_doichinova@mail.bg>
2021-12-22 00:02:37 +0200

    Add files via upload

---
+++ Selenium WebDriver lvl3 Homework/TestData/UserTestData.cs
@@ -0,0 +1,19 @@
+﻿using BasicSelenium_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSelenium_Project.TestData
+{
+    public class UserTestData
+    {
+        public static User DefaultIncorrectUser = new()
+        {
+            UserName = "WrongUserName",
+            Password = "WrongUserPassword"
+        };
+    }
+
+}
