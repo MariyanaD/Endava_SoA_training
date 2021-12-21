f86a12df58b2becdf3d37af1252bd15d8924bfa1
MariyanaD <m_doichinova@mail.bg>
2021-12-22 00:03:50 +0200

    Add files via upload

---
+++ Selenium WebDriver lvl3 Homework/WebPages/CookiesConsentPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+
+
+namespace BasicSelenium_Project
+{
+    class CookiesConsentPage
+    {
+        IWebDriver _driver;
+
+        public CookiesConsentPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private IWebElement IFrame => _driver.FindElement(By.CssSelector(""));
+        private IWebElement AcceptAllCookiesButton=> _driver.FindElement(By.CssSelector(""));
+
+        public void ClickAcceptAll()
+        {
+            _driver.SwitchTo().Frame(IFrame);
+            AcceptAllCookiesButton.Click();
+        }
+    }
+}
---
+++ Selenium WebDriver lvl3 Homework/WebPages/LoginPage.cs
@@ -0,0 +1,38 @@
+﻿
+using OpenQA.Selenium;
+
+
+namespace BasicSelenium_Project
+{
+    class LoginPage
+    {
+
+        IWebDriver _driver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            _driver = webDriver;
+        }
+
+        private IWebElement UserNameField => _driver.FindElement(By.XPath("//*[@name='email']"));
+        private IWebElement PasswordField => _driver.FindElement(By.XPath("//*[@name='pass']"));
+        private IWebElement SubmitButton => _driver.FindElement(By.XPath("//*[@type='submit']"));
+
+        public void EnterUserName(string userName)
+        {
+            UserNameField.Clear();
+            UserNameField.SendKeys(userName);
+        }
+
+        public void EnterPassword(string password)
+        {
+            PasswordField.Clear();
+            PasswordField.SendKeys(password);
+        }
+
+        public void ClickSubmitButton()
+        {
+            SubmitButton.Click();
+        }
+    }
+}
