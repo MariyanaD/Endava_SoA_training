f56ed9d5ef12c87b008450b1681be4a811731820
MariyanaD <m_doichinova@mail.bg>
2021-12-22 00:01:04 +0200

    Add files via upload

---
+++ Selenium WebDriver lvl3 Homework/Configuration/ConfigurationLabels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSelenium_Project.Configuration
+{
+    public static class ConfigurationLabels
+    {
+        public const string BaseUrl = "BaseUrl";
+        public static string DefaultImplicitTimeout = "DefaultImplicitTimeout";
+    }
+}
+    
+
+    
---
+++ Selenium WebDriver lvl3 Homework/Configuration/ConfigurationProviders.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace BasicSelenium_Project.Configuration
+{
+    static class ConfigurationProviders
+    {
+        public static IConfiguration GetValue { get; }
+        static ConfigurationProviders()
+        {
+            GetValue = new ConfigurationBuilder()
+                .SetBasePath($"{Directory.GetCurrentDirectory()}/Configuration")
+                .AddJsonFile("AppSettings.json")
+                .Build();
+        }
+    }
+}
+
+
+
